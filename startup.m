564b1ddac9a8b189b7eff7b86296daec6ce86e8c
Jasjeet Dhaliwal <jdhaliwal@vonmises.cs.umass.edu>
2017-05-11 20:14:37 -0400

    startup modified

--- startup.m
+++ startup.m
@@ -1,5 +1,9 @@
 function startup()
     curdir = fileparts(mfilename('fullpath'));
+    scripts_path = fullfile(curdir, 'scripts'); 
+    db_scripts_path = fullfile(curdir, 'db_scripts'); 
+    addpath(genpath(scripts_path));
+    addpath(genpath(db_scripts_path)); 
     mkdir_if_missing(fullfile(curdir, 'datasets'));
     caffe_path = fullfile(curdir,'caffe', 'matlab');
     if exist(caffe_path, 'dir') == 0
@@ -7,6 +11,5 @@ function startup()
     end
     addpath(genpath(caffe_path));
     mkdir_if_missing(fullfile(curdir, 'output'));
-    mkdir_if_missing(fullfile(curdir, 'models'));
     fprintf('seq2seq startup done\n');
 end
